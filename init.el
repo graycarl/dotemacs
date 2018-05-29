5a70dd9b6f5a2fef856e80802cee4f7d52a7ed9b
Hongbo He <hhbcarl@gmail.com>
2018-05-30 00:11:28 +0800

    Use emacsclient in term-mode

--- init.el
+++ init.el
@@ -9,25 +9,42 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Selected Package List
+(setq package-selected-packages
+      (quote (markdown-mode helm material-theme python
+              exec-path-from-shell)))
+
 ;; Setup PATH or `/usr/local/bin` will missed in PATH
 (when (memq window-system '(mac ns x)) (exec-path-from-shell-initialize))
 
-;; Some better default is setted by 'better-default' package
-;; see: https://github.com/technomancy/better-defaults/
-;; I don't like ido-mode, since it's shortcut can not be used all
-;; over the emacs (like icomplete-mode C-j).
-(require 'better-defaults nil t)
-(ido-mode -1)
+;; Setup GUI
+;; TODO: Do not hide menu bar in terminal-mode.
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
+;; Optimize some shotcuts
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+(setq-default indent-tabs-mode nil)
+(setq require-final-newline t)
+(setq visible-bell t)
 
 (icomplete-mode t)
 (setq inhibit-startup-screen t)
-(setq package-selected-packages
-      (quote (markdown-mode helm better-defaults material-theme python
-              exec-path-from-shell)))
 (setq tab-width 4)
 (xterm-mouse-mode t)
 (add-to-list 'default-frame-alist '(width . 180))
 (add-to-list 'default-frame-alist '(height . 35))
+
+;; Start server client
+(require 'server)
+(unless (server-running-p) (server-start))
+(setenv "EDITOR" "emacsclient")
 
 ;; Local config file
 (setq custom-file (expand-file-name "local.el" user-emacs-directory))
