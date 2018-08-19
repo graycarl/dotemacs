9a9d240b5f6f2f39f41ec8ebec7664bc6dbde1c8
Hongbo He <hhbcarl@gmail.com>
2018-08-19 20:12:44 +0800

    Optimize ido

--- config/ido.el
+++ config/ido.el
@@ -1,5 +1,14 @@
-;; Setup ido
-(ido-mode t)
+(setq ido-enable-prefix nil)
+(setq ido-use-virtual-buffers t)
 (setq ido-enable-flex-matching t)
+(setq ido-create-new-buffer 'always)
+(setq ido-use-filename-at-point 'guess)
+
+(after 'ido
+  (require-package 'flx-ido)
+  (flx-ido-mode t)
+
+  (require-package 'ido-vertical-mode)
+  (ido-vertical-mode))
 
 (provide 'config-ido)
