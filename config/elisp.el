fb427a7b18e6706305507385f324bbaa774a48f8
Hongbo He <hhbcarl@gmail.com>
2018-08-27 20:50:00 +0800

    Optimize fancy tab

--- config/elisp.el
+++ config/elisp.el
@@ -1,4 +1,13 @@
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             ;; Tab Completion
+            (setq hippie-expand-try-functions-list
+              '(try-complete-file-name-partially
+                try-complete-file-name
+                try-expand-all-abbrevs
+                try-expand-dabbrev
+                try-expand-dabbrev-all-buffers
+                try-expand-dabbrev-from-kill
+                try-complete-lisp-symbol-partially
+                try-complete-lisp-symbol))
             (local-set-key (kbd "TAB") 'fancy-tab)))
