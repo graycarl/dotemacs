77a02a2f45ee914787011e3d8c7aedbffd26e7f3
Hongbo He <hongbo.he@veeva.com>
2018-08-30 18:36:18 +0800

    Fix evil search

--- config/evil.el
+++ config/evil.el
@@ -4,7 +4,9 @@
 ;; Use C-u to scroll up instead of `universal-argument`
 (setq evil-want-C-u-scroll t)
 ;; Use evil-search instead of isearch
-(setq evil-search-module 'evil-search)
+;; TODO: press `n` to forward do not work as expected.
+;; Need to be fixed before using evil-search
+;; (setq evil-search-module 'evil-search)
 
 (after 'evil
 
@@ -18,6 +20,17 @@
 
   ;; Do not use clipboard when normal cut
   ;; (setq select-enable-clipboard nil)
+
+  ;; Fix search word
+  ;; See: https://emacs-china.org/t/evil-search-word-forward--/3340
+  (defun evil-search-symbol-backward ()
+    (interactive)
+    (evil-search-word-backward 1 t))
+  (defun evil-search-symbol-forward ()
+    (interactive)
+    (evil-search-word-forward 1 t))
+  (define-key evil-motion-state-map "#" #'evil-search-symbol-backward)
+  (define-key evil-motion-state-map "*" #'evil-search-symbol-forward)
   )
 
 (evil-mode t)
