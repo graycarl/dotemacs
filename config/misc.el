5b4cad877649109baa0b7a9efe073ddb93460877
Hongbo He <hongbo.he@veeva.com>
2018-09-26 13:56:31 +0800

    Add package htmlize

--- config/misc.el
+++ config/misc.el
@@ -1,7 +1,5 @@
 (require-package 'smex)
 (require-package 'youdao-dictionary)
-
-;; (require-package 'markdown-mode)
-;; (require-package 'python)
+(require-package 'htmlize)
 
 (provide 'config-misc)
